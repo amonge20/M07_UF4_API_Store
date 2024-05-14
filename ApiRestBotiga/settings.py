49bda8e2abaf23e3b7863cddf2fb4a79714c2ffc
Bilal El Moudden <belel22@jaumebalmes.net>
2024-05-14 19:31:39 +0200

    cuarto commit

--- ApiRestBotiga/settings.py
+++ ApiRestBotiga/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'carrito',
+    'catalogo',
+    'comandes',
+    'pago',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
