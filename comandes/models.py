dddb80d00a0bcd717201866c3d30e88525c9329f
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2024-05-21 19:13:04 +0200

    Mostrar historial de comandes

--- comandes/models.py
+++ comandes/models.py
@@ -1,3 +1,13 @@
+# models.py
+
 from django.db import models
 
-# Create your models here.
+class Comanda(models.Model):
+    id = models.AutoField(primary_key=True)
+    total_productos = models.PositiveIntegerField()
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comanda {self.id}'
---
+++ comandes/serializers.py
@@ -0,0 +1,8 @@
+# comandes/serializers.py
+from rest_framework import serializers
+from .models import Comanda
+
+class ComandaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comanda
+        fields = '__all__'
