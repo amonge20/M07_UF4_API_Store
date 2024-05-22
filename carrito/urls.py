1bd80751e5ece1e9e619cc8688cfe6378f85616a
amonge20 <2023_aitor.monge@iticbcn.cat>
2024-05-22 19:41:14 +0200

    Eliminar productes del carretó

--- carrito/urls.py
+++ carrito/urls.py
@@ -3,6 +3,7 @@ from carrito import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('createCarrito/', views.create_cart, name='create_cart'),
-    path('<int:cart_id>/addCarrito/', views.add_to_cart, name='add_to_cart'),
+    path('crearCarrito/', views.afegirCarrito, name='afegirCarrito'),
+    path('<int:cart_id>/añadirProductoCarrito/', views.afegirProducteCarrito, name='afegirProducteCarrito'),
+    path('<int:cart_id>/eliminarProductoCarrito/<int:product_id>/', views.eliminarProducteCarrito, name='eliminarProducteCarrito'),
 ]
--- carrito/views.py
+++ carrito/views.py
@@ -6,7 +6,7 @@ from .serializers import CartSerializer, AddCartItemSerializer
 
 # Añadir Carrito
 @api_view(['POST'])
-def create_cart(request):
+def afegirCarrito(request):
     client = None
     if request.user.is_authenticated:
         client = request.user
@@ -22,7 +22,7 @@ def create_cart(request):
 
 # Añadir Producto al Carrito
 @api_view(['POST'])
-def add_to_cart(request, cart_id):
+def afegirProducteCarrito(request, cart_id):
     serializer = AddCartItemSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -32,3 +32,32 @@ def add_to_cart(request, cart_id):
         }, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# Eliminar Producto del carrito
+@api_view(['DELETE'])
+def eliminarProducteCarrito(request, cart_id, product_id):
+    try:
+        cart = Cart.objects.get(id=cart_id, client=request.user, is_finalized=False)
+    except Cart.DoesNotExist:
+        return Response({
+            'status': 'error',
+            'message': 'Carrito no encontrado o ya finalizado.'
+        }, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        cart_item = CartItem.objects.get(cart=cart, product_id=product_id)
+    except CartItem.DoesNotExist:
+        return Response({
+            'status': 'error',
+            'message': 'Producto no encontrado en el carrito.'
+        }, status=status.HTTP_404_NOT_FOUND)
+    
+    cart_item.product.stock += cart_item.quantity
+    cart_item.product.save()
+    cart_item.delete()
+    
+    return Response({
+        'status': 'success',
+        'message': 'Producto eliminado del carrito.',
+        'data': CartSerializer(cart).data
+    }, status=status.HTTP_200_OK)
