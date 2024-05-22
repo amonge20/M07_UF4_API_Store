0188df3362a139d5ebb508c954b47a5d48099ac8
amonge20 <2023_aitor.monge@iticbcn.cat>
2024-05-22 19:56:51 +0200

    Modificar quantitat d’un producte

--- carrito/urls.py
+++ carrito/urls.py
@@ -3,8 +3,9 @@ from carrito import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('crearCarrito/', views.afegirCarrito, name='afegirCarrito'),
+    path('crearCarrito/', views.afegirCarrito, name='afegir_Carrito'),
     path('<int:cart_id>/añadirProductoCarrito/', views.afegirProducteCarrito, name='afegirProducteCarrito'),
     path('<int:cart_id>/eliminarProductoCarrito/<int:product_id>/', views.eliminarProducteCarrito, name='eliminarProducteCarrito'),
     path('<int:cart_id>/eliminarCarrito/', views.eliminarCarrito, name='eliminarCarrito'),
+    path('<int:cart_id>/modificarCantidadProducto/<int:product_id>/', views.modificarCantidadProducto, name='modificarCantidadProducto'),
 ]
--- carrito/views.py
+++ carrito/views.py
@@ -86,3 +86,49 @@ def eliminarCarrito(request, cart_id):
         'message': 'Carrito eliminado completamente.'
     }, status=status.HTTP_200_OK)
 
+# Modificar cantidad de un producto
+@api_view(['PUT'])
+def modificarCantidadProducto(request, cart_id, product_id):
+    try:
+        cart = Cart.objects.get(id=cart_id, client=request.user, is_finalized=False)
+    except Cart.DoesNotExist:
+        return Response({
+            'status': 'error',
+            'message': 'Carrito no encontrado o ya finalizado.'
+        }, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        cart_item = CartItem.objects.get(cart=cart, product_id=product_id)
+    except CartItem.DoesNotExist:
+        return Response({
+            'status': 'error',
+            'message': 'Producto no encontrado en el carrito.'
+        }, status=status.HTTP_404_NOT_FOUND)
+    
+    new_quantity = request.data.get('quantity')
+    if new_quantity is None or int(new_quantity) <= 0:
+        return Response({
+            'status': 'error',
+            'message': 'Cantidad no válida.'
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    new_quantity = int(new_quantity)
+    stock_difference = new_quantity - cart_item.quantity
+    
+    if cart_item.product.stock < stock_difference:
+        return Response({
+            'status': 'error',
+            'message': 'Stock insuficiente para el producto solicitado.'
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    cart_item.product.stock -= stock_difference
+    cart_item.product.save()
+    cart_item.quantity = new_quantity
+    cart_item.save()
+    
+    return Response({
+        'status': 'success',
+        'message': 'Cantidad del producto actualizada.',
+        'data': CartSerializer(cart).data
+    }, status=status.HTTP_200_OK)
+
