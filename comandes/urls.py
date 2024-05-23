05b7a6e94e56d5a3421913210e75f7d3a99bc947
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2024-05-23 18:41:26 +0200

    Mostrar historial de comandes que no estan finalitzades (correcion errores)

--- comandes/urls.py
+++ comandes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('historial/', views.historial_comandes, name='historial_comandes'),
     path('historial/cliente/<int:cliente_id>/', views.historial_comandes_cliente, name='historial_comandes_cliente'),
+    path('comandas-no-finalizadas/', views.comandas_no_finalizadas, name='comandas_no_finalizadas'),
 ]
--- comandes/views.py
+++ comandes/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Comanda, Cliente
 from .serializers import ComandaSerializer, ClienteSerializer
+from django.shortcuts import render
 
 @api_view(['GET'])
 def historial_comandes(request):
@@ -17,3 +18,12 @@ def historial_comandes_cliente(request, cliente_id):
     cliente_serializer = ClienteSerializer(cliente)
     comanda_serializer = ComandaSerializer(comandes, many=True)
     return Response({'cliente': cliente_serializer.data, 'comandes': comanda_serializer.data})
+
+@api_view(['GET'])
+def comandas_no_finalizadas(request):
+    try:
+        comandas = Comanda.objects.filter(finalizada=False)  # Filtrar comandas no finalizadas
+        serializer = ComandaSerializer(comandas, many=True)
+        return Response(serializer.data, status=200)
+    except Exception as e:
+        return Response({'message': 'Comandas no finalizadas'}, status=500)
